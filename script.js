68b9d9536c11152ac0412668bc1b70555d938f52
Anass Lamouri <anasslamouri@icloud.com>
2025-08-07 15:46:46 +0100

    Fixed typo in trailerContainerEl

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const btnEl = document.querySelector(".btn")
 const closeIconEl = document.querySelector(".close-icon")
-const trailerContainerEl = document.querySelector("trailer-container")
+const trailerContainerEl = document.querySelector(".trailer-container")
 
 function btnClicked() {
-  console.log("Button Clicked")
+  
 }
 
 btnEl.addEventListener("click", btnClicked)
