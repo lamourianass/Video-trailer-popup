f55730964a5601bb7ac0f71c6ca4b66ac322cdbb
Anass Lamouri <anasslamouri@icloud.com>
2025-08-07 15:42:01 +0100

    Add event listener to the btnEl

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const btnEl = document.querySelector(".btn")
 const closeIconEl = document.querySelector(".close-icon")
 const trailerContainerEl = document.querySelector("trailer-container")
 
+function btnClicked() {
+  console.log("Button Clicked")
+}
+
+btnEl.addEventListener("click", btnClicked)
